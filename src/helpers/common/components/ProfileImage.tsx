2b735fc2b0611ab2951d35b60da85649b6c25592
dhanushramudri <ramudridhanush@gmail.com>
2025-01-05 18:26:49 +0530

    profile image

--- src/helpers/common/components/ProfileImage.tsx
+++ src/helpers/common/components/ProfileImage.tsx
@@ -1,49 +1,27 @@
 import styled from '@emotion/styled';
 import { SectionValidator } from './ValidSectionRenderer';
 
-const ImageWrapper = styled.div`
-  position: relative;
-  display: inline-block;
-  aspect-ratio: 1/1;
-  overflow: hidden;
+const RoundedImage = styled.img`
   border-radius: 50%;
   border: 5px solid white;
-  flex-shrink: 0;
-`;
-
-const RoundedImage = styled.img`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
+  items-center;
 `;
 
 export const ProfileImage = ({
   src,
-  height,
-  width,
-  size = '108px',
+  height = '108px',
+  width = '108px',
   imageWrapperClassname = '',
 }: {
   src: string;
   height?: string;
   width?: string;
-  size?: string;
   imageWrapperClassname?: string;
 }) => {
-  // Use height or width if provided, otherwise fall back to size
-  const finalSize = height || width || size;
-
   return (
     <div className={imageWrapperClassname}>
       <SectionValidator value={src}>
-        <ImageWrapper
-          style={{
-            width: finalSize,
-            height: finalSize,
-          }}
-        >
-          <RoundedImage alt="Profile image" src={src} />
-        </ImageWrapper>
+        <RoundedImage alt="Profile image" src={src} height={height} width={width} />
       </SectionValidator>
     </div>
   );
