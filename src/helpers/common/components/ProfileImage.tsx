2f57a81a86e3c8f6fcee8309b7717f3e6955b90b
dhanushramudri <ramudridhanush@gmail.com>
2025-01-05 18:21:20 +0530

    profile image

--- src/helpers/common/components/ProfileImage.tsx
+++ src/helpers/common/components/ProfileImage.tsx
@@ -3,32 +3,27 @@ import { SectionValidator } from './ValidSectionRenderer';
 
 const ImageWrapper = styled.div`
   position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  display: inline-block;
+  aspect-ratio: 1/1;
   overflow: hidden;
   border-radius: 50%;
   border: 5px solid white;
+  flex-shrink: 0; // Prevents shrinking
 `;
 
 const RoundedImage = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
-  position: absolute;
-  top: 0;
-  left: 0;
 `;
 
 export const ProfileImage = ({
   src,
-  height = '108px',
-  width = '108px',
+  size = '108px', // Single size prop instead of separate width/height
   imageWrapperClassname = '',
 }: {
   src: string;
-  height?: string;
-  width?: string;
+  size?: string;
   imageWrapperClassname?: string;
 }) => {
   return (
@@ -36,12 +31,8 @@ export const ProfileImage = ({
       <SectionValidator value={src}>
         <ImageWrapper
           style={{
-            width,
-            height,
-            minWidth: width,
-            minHeight: height,
-            maxWidth: width,
-            maxHeight: height,
+            width: size,
+            height: size,
           }}
         >
           <RoundedImage alt="Profile image" src={src} />
